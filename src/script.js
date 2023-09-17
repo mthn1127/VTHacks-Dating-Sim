0d86424c737fb12e7f57d6db1ff9d74b789bbcc5
mthn1127 <minhthu@vt.edu>
2023-09-17 03:19:56 -0400

    yippieeee

--- src/script.js
+++ src/script.js
@@ -1,14 +1,16 @@
-var name;
 var pronoun;
 
 function setIdentity(){
-    name = document.getElementById("name").value;
-    var pronoun_options = document.getElementsByName('pronoun');
+
+    var nameInput = document.getElementById("nameInput");
     for (i = 0; i < pronoun_options.length; i++) {
         if (pronoun_options[i].checked){
             pronoun = pronoun_options[i].value;
         }
     }
-    
-
+    var myForm = document.getElementById("myForm");
+    myForm.addEventListener("submit", function(event) {
+        event.preventDefault();
+    });
+    localStorage.setItem("fullName", fullName);
 }
